69b09e6172f1fd2fc06f3bcdd2fe800dddcdd2fa
Xinru Chen <tonytonytony0724@gmail.com>
2019-09-19 21:29:14 +1200

    bug: loadcourse not working

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,46 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-import { createCourse } from "../../redux/actions/courseActions";
+import { createCourse, loadCourses } from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
-    }
-  };
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.createCourse(this.state.course);
+  ComponentDidMount = () => {
+    return <h1>hi</h1>;
+    this.props.loadCourses().catch(error => {
+      throw error;
+    });
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => {
           return <div key={course.title}>{course.title}</div>;
         })}
-      </form>
+      </>
     );
   }
 }
 
 CoursesPage.propTypes = {
+  loadCourses: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired,
   createCourse: PropTypes.func.isRequired
 };
@@ -50,7 +34,8 @@ function mapStateToProps({ courses }) {
 }
 
 const mapDispatchToProps = {
-  createCourse
+  createCourse,
+  loadCourses
 };
 
 export default connect(
